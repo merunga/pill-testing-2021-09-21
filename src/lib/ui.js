48f806f7450af71059854f31f99c586dfdd8d575
merunga <merunga@gmail.com>
2021-09-21 12:05:03 +0200

    quitar elementos no necesarios del arbol del DOM

--- src/lib/ui.js
+++ src/lib/ui.js
@@ -23,57 +23,59 @@ export const postDetails = (props) => {
   const showPost = post && !editing;
   const noShow = !post && !editing;
 
-  const divElem = document.createElement('div');
+  const formElem = document.createElement('form');
+  formElem.classList.add('mt-2');
+  if (newPost || editPost) {
+    formElem.id = 'post-form';
+  }
 
   const btnClasses = 'my-6 mx-1 w-full border border-transparent rounded-md shadow-sm py-2 px-4 text-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
 
   if (!noShow) {
-    divElem.innerHTML = `
-      <form ${newPost || editPost ? 'id="post-form"' : ''} class="mt-6">
-        <h2 class="text-lg font-medium text-gray-900">
-          ${newPost ? 'Nuevo post' : ''}
-          ${editPost ? 'Editar post' : ''}
-          ${showPost
-            ? `Post del ${post.dateCreated.toDate().toISOString().slice(0, 16).replace('T', ' a las ')}
-              ${post && post.dateLastEdited
-                ? `<br /><small class="text-sm">Última edición: ${post.dateLastEdited.toDate().toISOString().slice(0, 16).replace('T', ' a las ')}</small>`
-                : ''
-              }
-              `
-            : ''
-          }
-        </h2>
-
-        ${post && post.id
-          ? `<input id="post-id" type="hidden" value="${post.id}"></input>`
+    formElem.innerHTML = `
+      <h2 class="text-lg font-medium text-gray-900">
+        ${newPost ? 'Nuevo post' : ''}
+        ${editPost ? 'Editar post' : ''}
+        ${showPost
+          ? `Post del ${post.dateCreated.toDate().toISOString().slice(0, 16).replace('T', ' a las ')}
+            ${post && post.dateLastEdited
+              ? `<br /><small class="text-sm">Última edición: ${post.dateLastEdited.toDate().toISOString().slice(0, 16).replace('T', ' a las ')}</small>`
+              : ''
+            }
+            `
           : ''
         }
-        ${editing
-          ? (`
-            <textarea id="post-text" name="post-text"
-              class="mt-6 block w-full border-gray-300 rounded-xs p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            >${(post && post.text) || ''}</textarea>`
-          )
-          : (
-            `<p class="mt-6 w-full p-1 text-xl">${post && post.text}</p>`
-          )
-        }
+      </h2>
+
+      ${post && post.id
+        ? `<input id="post-id" type="hidden" value="${post.id}"></input>`
+        : ''
+      }
+      ${editing
+        ? (`
+          <textarea id="post-text" name="post-text"
+            class="mt-6 block w-full border-gray-300 rounded-xs p-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          >${(post && post.text) || ''}</textarea>`
+        )
+        : (
+          `<p class="mt-6 w-full p-1 text-xl">${post && post.text}</p>`
+        )
+      }
 
 
-        <div class="flex">
-          ${newPost ? `<button id="btn-crear" class="${btnClasses} bg-indigo-600 hover:bg-indigo-700 text-white">Crear</button>` : ''}
-          ${editPost ? `<button id="btn-guardar" class="${btnClasses} bg-indigo-600 hover:bg-indigo-700 text-white">Guardar</button>` : ''}
-          ${editPost ? `<button id="btn-cancelar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Cancelar</button>` : ''}
-          ${showPost ? `<button id="btn-editar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Editar</button>` : ''}
-          ${showPost ? `<button id="btn-eliminar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Eliminar</button>` : ''}
-        </div>
-      </form>
+      <div class="flex">
+        ${newPost ? `<button id="btn-crear" class="${btnClasses} bg-indigo-600 hover:bg-indigo-700 text-white">Crear</button>` : ''}
+        ${editPost ? `<button id="btn-guardar" class="${btnClasses} bg-indigo-600 hover:bg-indigo-700 text-white">Guardar</button>` : ''}
+        ${editPost ? `<button id="btn-cancelar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Cancelar</button>` : ''}
+        ${showPost ? `<button id="btn-editar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Editar</button>` : ''}
+        ${showPost ? `<button id="btn-eliminar" class="${btnClasses} bg-gray-300 hover:bg-gray-400 text-black">Eliminar</button>` : ''}
+      </div>
     `;
 
     if (newPost) {
-      divElem.querySelector('#btn-crear').addEventListener('click', (e) => {
+      formElem.querySelector('#btn-crear').addEventListener('click', (e) => {
         e.preventDefault();
-        const textArea = divElem.querySelector('#post-text');
+        const textArea = formElem.querySelector('#post-text');
         if (textArea.value.trim()) {
           createPost(textArea.value).then(renderPage);
           textArea.value = '';
@@ -82,16 +84,16 @@ export const postDetails = (props) => {
     }
 
     if (editPost) {
-      divElem.querySelector('#btn-guardar').addEventListener('click', (e) => {
+      formElem.querySelector('#btn-guardar').addEventListener('click', (e) => {
         e.preventDefault();
-        const textArea = divElem.querySelector('#post-text');
-        const idInput = divElem.querySelector('#post-id');
+        const textArea = formElem.querySelector('#post-text');
+        const idInput = formElem.querySelector('#post-id');
         if (textArea.value.trim()) {
           updatePost(idInput.value, textArea.value).then(renderPage);
           textArea.value = '';
         }
       });
-      divElem.querySelector('#btn-cancelar').addEventListener('click', (e) => {
+      formElem.querySelector('#btn-cancelar').addEventListener('click', (e) => {
         e.preventDefault();
         const root = document.getElementById('root');
         const oldForm = root.querySelector('#post-form');
@@ -101,24 +103,24 @@ export const postDetails = (props) => {
     }
 
     if (showPost) {
-      divElem.querySelector('#btn-editar').addEventListener('click', (e) => {
+      formElem.querySelector('#btn-editar').addEventListener('click', (e) => {
         e.preventDefault();
         const root = document.getElementById('root');
         const oldForm = root.querySelector('#post-form');
         const newForm = postDetails({ post, editing: true });
         oldForm.parentNode.replaceChild(newForm, oldForm);
       });
-      divElem.querySelector('#btn-eliminar').addEventListener('click', (e) => {
+      formElem.querySelector('#btn-eliminar').addEventListener('click', (e) => {
         e.preventDefault();
         const result = window.confirm('¿Eliminar?');
         if (result) {
-          const idInput = divElem.querySelector('#post-id');
+          const idInput = formElem.querySelector('#post-id');
           deletePost(idInput.value).then(renderPage);
         }
       });
     }
   }
-  return divElem;
+  return formElem;
 };
 
 export const postList = (props) => {
@@ -141,7 +143,7 @@ export const postList = (props) => {
   const ul = divElem.querySelector('ul');
   posts.forEach((p) => {
     const li = document.createElement('li');
-    li.innerHTML = '<li class="flex py-1"></li>';
+    li.outerHTML = '<li class="flex py-1"></li>';
     li.appendChild(postDetails({ post: p }));
     ul.appendChild(li);
   });
